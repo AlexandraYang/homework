79b11dd62ea37eb4c45d50901fa1814203f78038
AlexandraYang <shiuanyang122@gmail.com>
2023-05-06 19:42:34 +0800

    Finish Search function

--- java_hw/MainProgram.java
+++ java_hw/MainProgram.java
@@ -6,7 +6,6 @@ class MainProgram{
         Scanner scan = new Scanner(System.in);
         String selection;
 
-        Person p1 = new Person("Jaime", "Amherst", "000-000-0000");
         Persons ps1 = new Persons();
 
         do {
@@ -27,25 +26,31 @@ class MainProgram{
                     System.out.println();
                     break;
                 case "2":
-                    enterNewPerson(ps1);
-                    System.out.println("二");
-                    break; 
+                    enterNewPerson(ps1, scan);
+                    break;
                 case "3":
-                    System.out.println("三");
-                    break; 
+                    System.out.println("Enter the name that you want to search");
+                    scan.nextLine();
+                    String letter = scan.nextLine();
+                    ps1.search(ps1.getArrayList(),letter);
+                    break;
                 case "4":
                     System.out.println("四");
                     break;
                 case "5":
                     System.out.println("五");
-                    break;   
+                    break;
                 case "Q":
                     System.out.println("終止");
                 }
         } while (!"Q".equals(selection));
+
+        // close scan
+        scan.close();
     }
 
     public static void enterNewPerson(Persons P, Scanner scanner) {
+        scanner.nextLine();
         System.out.println("Enter name: ");
         String name = scanner.nextLine();
         System.out.println("Enter address: ");
@@ -60,18 +65,29 @@ class MainProgram{
             int year = scanner.nextInt();
             scanner.nextLine(); // consume the newline character
             Student personCreated = new Student(name, address, phone, year);
+            P.add(personCreated);
+            // ALEX DEBUG
+            System.out.println("Student " + name + " " + address + " " + phone + " " + year +  "is added");
         }
         else {
             System.out.println("Is the person an employee? (y/n)");
-            String answer1 = scanner.nextLine();
-            if (answer.equalsIgnoreCase("y")) {
+            String isEmployee = scanner.nextLine();
+            if (isEmployee.equalsIgnoreCase("y")) {
                 System.out.println("Enter department: ");
                 String department = scanner.nextLine();
                 scanner.nextLine();
                 Employee personCreated = new Employee(name, address, phone, department);
+                P.add(personCreated);
+
+                // ALEX DEBUG
+                System.out.println("Employee " + name + " " + address + " " + phone + " " + department + " is added");
+
             }
             else{
                 Person personCreated = new Person(name, address, phone);
+                P.add(personCreated);
+                // ALEX DEBUG
+                System.out.println("Person " + name + " " + address + " " + phone + " " + " is added");
             }
         }
     }
